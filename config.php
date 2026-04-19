91bdd4183646fdc88bbcbeaa602b513d49c65c83
dararamadhani <dara37@mhs.usk.ac.id>
2026-04-20 00:11:11 +0700

    Refactor database connection in config.php
    Refactor database connection code to use a more structured approach and return error as JSON.

--- config.php
+++ config.php
@@ -1,11 +1,11 @@
 <?php
-$host = "sql111.infinityfree.com"; 
-$user = "if0_41702193";            
-$pass = "F9SmpAUkjpqXuX";           
-$db   = "if0_12345678_portfolio_kontak"; 
-
-$conn = new mysqli($host, $user, $pass, $db);
+$conn = new mysqli(
+    "sql111.infinityfree.com",
+    "if0_41702193",
+    "F9SmpAUkjpqXuX",
+    "if0_41702193_portfolio_kontak"
+);
 if ($conn->connect_error) {
-    die("Koneksi gagal: " . $conn->connect_error);
+    die(json_encode(["error" => $conn->connect_error]));
 }
 ?>

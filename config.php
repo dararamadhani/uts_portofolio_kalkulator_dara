77caf1a3de82c61cc4089e90ef41785857738e1e
dararamadhani <dara37@mhs.usk.ac.id>
2026-04-20 00:01:27 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,10 @@
 <?php
-$conn = new mysqli("localhost", "root", "", "portfolio_kontak");
+$host = "sql111.infinityfree.com"; 
+$user = "if0_41702193";            
+$pass = "F9SmpAUkjpqXuX";           
+$db   = "if0_12345678_portfolio_kontak"; 
 
+$conn = new mysqli($host, $user, $pass, $db);
 if ($conn->connect_error) {
     die("Koneksi gagal: " . $conn->connect_error);
 }
